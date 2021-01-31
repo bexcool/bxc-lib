7151b7d8dc4d88d6a79680af84b8a786e0c7149e
Spej <Jiriksperlik@seznam.cz>
2021-01-31 17:01:28 +0100

    Update SmartMesage.js
    Now uses es5

--- js/SmartMesage.js
+++ js/SmartMesage.js
@@ -1,64 +1,72 @@
 "use strict";
-class SmartMessage {
-    static get unexpected_error() {
-        return "216unexpected_error";
+var SmartMessage = /** @class */ (function () {
+    function SmartMessage() {
     }
-    static serialize(msg) {
-        const len = msg.length;
-        const len_size = len.toString().length;
+    Object.defineProperty(SmartMessage, "unexpected_error", {
+        get: function () {
+            return "216unexpected_error";
+        },
+        enumerable: false,
+        configurable: true
+    });
+    SmartMessage.serialize = function (msg) {
+        var len = msg.length;
+        var len_size = len.toString().length;
         if (len_size.toString().length != 1)
             console.error(this.unexpected_error + "Size of message length must be 1 byte");
         return len_size.toString() + len.toString() + msg;
-    }
+    };
     /**
      * Parsing
      * get size of message length (e.g. size 2 for a message with length 16)
      */
-    static messageLengthSize(msg) {
-        const len_size = parseInt(msg.substr(0, 1));
+    SmartMessage.messageLengthSize = function (msg) {
+        var len_size = parseInt(msg.substr(0, 1));
         if (len_size <= 0 && !isNaN(len_size))
             console.error(this.unexpected_error +
                 "Size of message length must be a number and cannot be less or equal to 0");
         return len_size;
-    }
+    };
     /**
      * Parsing
      * get length of a message
      */
-    static messageLength(msg, len_size = -1) {
+    SmartMessage.messageLength = function (msg, len_size) {
+        if (len_size === void 0) { len_size = -1; }
         // don't calculate the size if the user already has it somewhere
         if (len_size == -1)
             len_size = this.messageLengthSize(msg);
         // offset 1 since that's the position of the size
-        const len = parseInt(msg.substr(1, len_size));
+        var len = parseInt(msg.substr(1, len_size));
         return len;
-    }
+    };
     /**
      * Parsing
      * get length of message, message size and 1 for the size byte (combined)
      */
-    static messageTotalLength(msg) {
-        const len_size = this.messageLengthSize(msg);
+    SmartMessage.messageTotalLength = function (msg) {
+        var len_size = this.messageLengthSize(msg);
         // offset 1 since that's the position of the size
-        const len = this.messageLength(msg, len_size);
+        var len = this.messageLength(msg, len_size);
         return 1 + len_size + len;
-    }
+    };
     /**
      * Parsing
      * remove the size, length and message from a string
      */
-    static removeMessage(msg) {
+    SmartMessage.removeMessage = function (msg) {
         return msg.substr(this.messageTotalLength(msg));
-    }
+    };
     /**
      * Parsing
      * get the message from a string
      */
-    static getMessage(msg) {
-        const len_size = this.messageLengthSize(msg);
+    SmartMessage.getMessage = function (msg) {
+        var len_size = this.messageLengthSize(msg);
         // offset 1 since that's the position of the size
-        const len = this.messageLength(msg, len_size);
+        var len = this.messageLength(msg, len_size);
         // read $len chars after the size and length
         return msg.substr(1 + len_size, len);
-    }
-}
+    };
+    return SmartMessage;
+}());
